35a92a0918743fd93ae58a8324240c4bdc5dd493
thomastbl <101262386+thomastbl@users.noreply.github.com>
2025-12-04 17:42:46 +0100

    test: add greeting

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -13,6 +13,7 @@ const firstName = "Carlos";
 const lastName = "Stevenson";
 const thisYear = 1965;
 const birthYear = 1947;
+const greeting = `Hello! My name is ${firstName} ${lastName} and I am ${thisYear-birthYear} years old.`
 
 
 
