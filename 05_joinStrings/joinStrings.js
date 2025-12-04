1d683c7b655627d5ca73061f8229dc12ede6ed13
thomastbl <101262386+thomastbl@users.noreply.github.com>
2025-12-04 17:47:40 +0100

    feat: complete joinStrings exercise

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -13,9 +13,12 @@ const firstName = "Carlos";
 const lastName = "Stevenson";
 const thisYear = 1965;
 const birthYear = 1947;
-const greeting = `Hello! My name is ${firstName} ${lastName} and I am ${thisYear-birthYear} years old.`;
 
 const fullName = firstName +" "+ lastName;
+const age = thisYear - birthYear;
+
+const greeting = `Hello! My name is ${fullName} and I am ${age} years old.`;
+
 
 
 
