fc7c77468007daae6f50ae1c96a46d9b523778ac
thomastbl <101262386+thomastbl@users.noreply.github.com>
2026-01-15 11:44:03 +0100

    test: function returns ERROR when the number input is negative

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,9 +1,13 @@
 const repeatString = function (string, num) {
-  let finalString = "";
-  for (let i = 0; i < num; i++) {
-    finalString = finalString.concat("", string);
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    let finalString = "";
+    for (let i = 0; i < num; i++) {
+      finalString = finalString.concat("", string);
+    }
+    return finalString;
   }
-  return finalString;
 };
 
 // Do not edit below this line
