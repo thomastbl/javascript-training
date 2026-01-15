9f2ec3cfc43048d14517d5666eddc6233523a8d4
thomastbl <101262386+thomastbl@users.noreply.github.com>
2026-01-15 11:39:28 +0100

    test: ensure repeatString does not use repeat method

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function (string, num) {
-  return string.repeat(num);
+  let finalString = "";
+  for (let i = 0; i < num; i++) {
+    finalString = finalString.concat("", string);
+  }
+  return finalString;
 };
 
 // Do not edit below this line
--- 06_repeatString/repeatString.spec.js
+++ 06_repeatString/repeatString.spec.js
@@ -15,7 +15,7 @@ describe("repeatString", () => {
   test("repeats the string 0 times", () => {
     expect(repeatString("bye", 0)).toEqual("");
   });
-  test.skip("does not use the built-in String repeat method", () => {
+  test("does not use the built-in String repeat method", () => {
     /* Even though there is a built-in String repeat method,
       in this exercise specifically, we want you to practise using loops */
     jest
