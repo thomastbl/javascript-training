88cb467ba95c8fbd23ee9e91fd2c85671451360e
thomastbl <101262386+thomastbl@users.noreply.github.com>
2026-01-16 11:23:39 +0100

    test: sumsAll sums numbers within the range

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (numA, numB) {
+  let result = 0;
+  for (let i = numA; i <= numB; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
