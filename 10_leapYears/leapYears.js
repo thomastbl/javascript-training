452460de4963e587685b63c01b066aefc663ee0d
thomastbl <101262386+thomastbl@users.noreply.github.com>
2026-01-16 18:26:39 +0100

    feat: leapYears returns leap years

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,5 @@
 const leapYears = function (year) {
-  if (year % 4 === 0 && year % 100 !== 0) return true;
+  if ((year % 4 === 0) & (year % 100 !== 0) || year % 400 === 0) return true;
   else return false;
 };
 
