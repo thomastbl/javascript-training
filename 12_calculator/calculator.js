1060523af0525ba7cd415171aad8f35532f6d7f0
thomastbl <101262386+thomastbl@users.noreply.github.com>
2026-03-30 11:58:25 +0200

    complete sum function

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -2,9 +2,15 @@ const add = function (numA, numB) {
   return numA + numB;
 };
 
-const subtract = function () {};
+const subtract = function (numA, numB) {
+  return numA - numB;
+};
 
-const sum = function () {};
+const sum = function (array) {
+  return array.reduce((acc, value) => {
+    return acc + value;
+  }, 0);
+};
 
 const multiply = function () {};
 
