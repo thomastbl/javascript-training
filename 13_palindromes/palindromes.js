894afd66e397199a7f6122f47d8f4990860973a4
thomastbl <101262386+thomastbl@users.noreply.github.com>
2026-03-30 17:32:24 +0200

    complete palindromes function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // Je supprime tout ce qui n'est pas une lettre et la passe en minuscule
+  clearedString = string.replace(/\W/gi, "").toLowerCase();
+  // Je sépare - reverse - et join à nouveau
+  reversedClearedString = clearedString.split("").reverse().join("");
+  // Je compare les deux strings
 
+  return clearedString === reversedClearedString;
 };
 
 // Do not edit below this line
