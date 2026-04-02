0a4bd7d6475ae9e91da5dd1084606840ac4ccf4b
thomastbl <101262386+thomastbl@users.noreply.github.com>
2026-04-02 15:10:51 +0200

    complete fibonacci function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n_number) {
+  let fibonacciArray = [0, 1];
+  n_number = Number(n_number);
 
+  if (n_number >= 0) {
+    for (let n = 2; n <= n_number; n++) {
+      fibonacciArray[n] = fibonacciArray[n - 1] + fibonacciArray[n - 2];
+    }
+
+    return fibonacciArray[n_number];
+  } else {
+    return "OOPS";
+  }
 };
-
+// one - two - n
 // Do not edit below this line
 module.exports = fibonacci;
