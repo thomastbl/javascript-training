fed49637f5c9928b10161b6c31efefbe7651db56
thomastbl <101262386+thomastbl@users.noreply.github.com>
2026-04-02 14:25:56 +0200

    fix: add const declarations to variables

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,8 +1,8 @@
 const palindromes = function (string) {
   // Je supprime tout ce qui n'est pas une lettre et la passe en minuscule
-  clearedString = string.replace(/\W/gi, "").toLowerCase();
+  const clearedString = string.replace(/\W/gi, "").toLowerCase();
   // Je sépare - reverse - et join à nouveau
-  reversedClearedString = clearedString.split("").reverse().join("");
+  const reversedClearedString = clearedString.split("").reverse().join("");
   // Je compare les deux strings
 
   return clearedString === reversedClearedString;
