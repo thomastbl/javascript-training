2e18d8f00b611ca78cc60ee6f85f08085b2e71df
thomastbl <101262386+thomastbl@users.noreply.github.com>
2026-04-08 17:52:26 +0200

    complete findTheOldest function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  return array.reduce((acc, currentPerson) => {
+    if (acc.yearOfDeath === undefined) {
+      acc.yearOfDeath = new Date().getFullYear();
+    }
+    if (currentPerson.yearOfDeath === undefined) {
+      currentPerson.yearOfDeath = new Date().getFullYear();
+    }
 
+    if (
+      acc.yearOfDeath - acc.yearOfBirth <
+      currentPerson.yearOfDeath - currentPerson.yearOfBirth
+    ) {
+      return currentPerson;
+    } else {
+      return acc;
+    }
+  }, array[0]);
 };
 
 // Do not edit below this line
